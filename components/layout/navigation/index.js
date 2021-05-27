475292350177bf346e51daf65b63a33174b4bac1
wang-jintao <wangjintaoapp@gmail.com>
2021-05-27 18:45:29 +0800

    更新URL

--- components/layout/navigation/index.js
+++ components/layout/navigation/index.js
@@ -37,7 +37,7 @@ const Navigation = () => {
       },
     });
 
-    win.loadURL('/today').then((r) => console.log('load success!'));
+    win.loadURL('https://gtd-todo.vercel.app/today').then((r) => console.log('load success!'));
     win.on('close', () => {
       win = null;
     });
